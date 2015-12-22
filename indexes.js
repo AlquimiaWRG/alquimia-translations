3cab126c026fd1828e0176a29945eaf11364ce93
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2015-12-22 11:39:06 +0100

    Update indexes filter

---
+++ indexes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = function(defaults) {
+  defaults.getElement('angular').push('angular-gettext', 'angular-cookies');
+
+  return defaults;
+};
--- jsIndex.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = function(defaultJs) {
-  defaultJs.getElement('angular').push('angular-gettext', 'angular-cookies');
-  defaultJs.getElement('modules').push('gettext');
-
-  return defaultJs;
-};
